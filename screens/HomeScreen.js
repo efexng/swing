4f90631501d43df0fb61b9e639b8bf2a84de47ae
efexng <efeapoki2020@gmail.com>
2024-05-11 03:15:07 +0100

    changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,30 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Modal } from 'react-native';
+import { Text, View, TouchableOpacity, Modal, ScrollView } from 'react-native'; // Include ScrollView from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { useVideoPlayer, VideoView } from 'expo-video';
-import { ScrollView } from 'react-native-gesture-handler';
+import styles from './HomeScreenStyle';
 
-const videoSource = 'https://res.cloudinary.com/dsspatqxn/video/upload/v1715284562/m0n6tqk9b6jqaliqvmho.mp4';
+const videoData = [
+  {
+    id: 1,
+    source: 'https://res.cloudinary.com/dsspatqxn/video/upload/v1715284562/m0n6tqk9b6jqaliqvmho.mp4',
+    title: 'BEETLEJUICE BEETLEJUICE _ Official Teaser Trailer',
+    description: 'In Cinemas on January 19',
+  },
+  {
+    id: 2,
+    source: 'https://res.cloudinary.com/dsspatqxn/video/upload/v1715367171/vtdpihagqxlurrymvxch.mp4',
+    title: 'BEETLEJUICE BEETLEJUICE _ Official Teaser Trailer',
+    description: 'In Cinemas on January 19',
+  },
+  {
+    id: 3,
+    source: 'https://res.cloudinary.com/dsspatqxn/video/upload/v1715373759/etsxpjmodymbmnngp4ip.mp4',
+    title: 'BEETLEJUICE BEETLEJUICE _ Official Teaser Trailer',
+    description: 'In Cinemas on January 19',
+  },
+];
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -13,48 +32,36 @@ const HomeScreen = () => {
   const [isVolumeMuted, setIsVolumeMuted] = useState(false);
   const [isLiked, setIsLiked] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [likeCount, setLikeCount] = useState(0); // Track like count
-  const [isPlaying, setIsPlaying] = useState(true); // Track video playing state
-  const player = useVideoPlayer(videoSource, (player) => {
-    player.loop = true;
-    player.play();
-  });
+  const [likeCount, setLikeCount] = useState({});
+  const [isPlaying, setIsPlaying] = useState({});
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [activeHeader, setActiveHeader] = useState('Cinema');
+
+  const players = videoData.map(video =>
+    useVideoPlayer(video.source, player => {
+      player.loop = false;
+      player.play();
+      setLikeCount(prevState => ({ ...prevState, [video.id]: 0 }));
+      setIsPlaying(prevState => ({ ...prevState, [video.id]: true }));
+    })
+  );
 
   const toggleModal = () => {
     setIsModalVisible(!isModalVisible);
   };
 
-  useEffect(() => {
-    const subscription = player.addListener('playingChange', (isPlaying) => {
-      setIsPlaying(isPlaying);
-    });
-
-    return () => {
-      subscription.remove();
-    };
-  }, [player]);
-
-  const toggleVolume = () => {
-    setIsVolumeMuted(!isVolumeMuted);
-    player.muted = !isVolumeMuted; // Toggle the muted state of the player
+  const toggleLike = videoId => {
+    const updatedLikeCount = isLiked[videoId] ? likeCount[videoId] - 1 : likeCount[videoId] + 1;
+    setLikeCount(prevState => ({ ...prevState, [videoId]: updatedLikeCount }));
+    setIsLiked(prevState => ({ ...prevState, [videoId]: !prevState[videoId] }));
   };
 
-  const toggleLike = () => {
-    setIsLiked(!isLiked);
-    setLikeCount(isLiked ? likeCount - 1 : likeCount + 1); // Update like count based on like state
+  const toggleVolume = videoId => {
+    setIsVolumeMuted(prevState => ({ ...prevState, [videoId]: !prevState[videoId] }));
+    players[videoId - 1].muted = !isVolumeMuted[videoId];
   };
 
-  const handlePlayPause = () => {
-    setIsPlaying(!isPlaying); // Toggle play/pause state
-    if (!isPlaying) {
-      player.play();
-    } else {
-      player.pause();
-    }
-  };
-
-  // Format the like count for display
-  const formatLikeCount = (count) => {
+  const formatLikeCount = count => {
     if (count < 1000) {
       return count.toString();
     } else if (count < 1000000) {
@@ -68,61 +75,90 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.videocontainer}>
-        <TouchableOpacity onPress={handlePlayPause} style={styles.video}>
-          <VideoView
-            ref={ref}
-            style={styles.video}
-            player={player}
-            contentFit="cover"
-            contentPosition="center"
-            nativeControls={false}
-            shouldPlay={isPlaying}
-            resizeMode="cover"
-          />
-        </TouchableOpacity>
-        <View style={styles.videocontrols}>
-          <TouchableOpacity onPress={toggleLike}>
-            <Ionicons
-              name={isLiked ? 'heart' : 'heart-outline'}
-              size={30}
-              color={isLiked ? 'red' : '#fff'}
+      <ScrollView
+        style={styles.videoScrollView} // Add styles for the ScrollView
+        showsVerticalScrollIndicator={false} // Hide vertical scroll indicator
+        pagingEnabled
+      >
+        {videoData.map((video, index) => (
+          <View key={video.id} style={styles.videocontainer}>
+            <View  style={{backgroundColor: 'black', borderRadius: 5,}}>
+            <VideoView
+              ref={ref}
+              style={{ height: 790, borderRadius: 10, }}
+              player={players[index]}
+              contentFit="cover"
+              contentPosition="center"
+              nativeControls={false}
+              shouldPlay={isPlaying[index]}
+              resizeMode="cover"
             />
-            {likeCount > 0 && (
-              <Text style={styles.likeCountText}>{formatLikeCount(likeCount)}</Text>
-            )}
-          </TouchableOpacity>
-          <TouchableOpacity onPress={toggleVolume}>
-            <Ionicons
-              name={isVolumeMuted ? 'volume-mute-outline' : 'volume-high-outline'}
-              size={30}
-              color="#fff"
-            />
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={toggleModal}>
-            <Ionicons
-              name="ellipsis-vertical-outline"
-              size={30}
-              color="#fff"
-            />
-          </TouchableOpacity>
-
-        </View>
-      </View>
-
-      <View style={styles.headercontainer}>
-      <Text style={styles.headertitle}>What's Coming</Text>
-
-      <ScrollView horizontal={true}>
-      <Text style={styles.headercontents}>Cinema</Text>  
-      <Text style={styles.headercontents}>Netflix</Text>  
-      <Text style={styles.headercontents}>HBO</Text>  
-      <Text style={styles.headercontents}>Disney +</Text>  
-      <Text style={styles.headercontents}>Showmax</Text>  
-      <Text style={styles.headercontents}>Apple Tv</Text>  
+            <View style={styles.controlsContainer}>
+              <TouchableOpacity
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  if (isPlaying[index]) {
+                    players[index].pause();
+                  } else {
+                    players[index].play();
+                  }
+                  setIsPlaying(prevState => ({ ...prevState, [index]: !prevState[index] }));
+                }}
+              />
+            </View>
+            <View style={styles.videodetails}>
+              <Text style={styles.videodetailstext}>{video.title}</Text>
+              <Text style={styles.videodetailstext2}>{video.description}</Text>
+            </View>
+            <View style={styles.videocontrols}>
+              <TouchableOpacity onPress={() => toggleLike(video.id)}>
+                <Ionicons
+                  name={isLiked[video.id] ? 'heart' : 'heart-outline'}
+                  size={30}
+                  color={isLiked[video.id] ? 'red' : '#fff'}
+                />
+                {likeCount[video.id] > 0 && (
+                  <Text style={styles.likeCountText}>{formatLikeCount(likeCount[video.id])}</Text>
+                )}
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => toggleVolume(video.id)}>
+                <Ionicons
+                  name={isVolumeMuted[video.id] ? 'volume-mute-outline' : 'volume-high-outline'}
+                  size={30}
+                  color="#fff"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={toggleModal}>
+                <Ionicons
+                  name="ellipsis-vertical-outline"
+                  size={30}
+                  color="#fff"
+                />
+              </TouchableOpacity>
+            </View>
+            </View>
+          </View>
+        ))}
       </ScrollView>
 
+       <View style={styles.headercontainer}>
+        <Text style={styles.headertitle}>What's Coming</Text>
+        <ScrollView horizontal={true} style={styles.headerScrollView}>
+          {['Cinema', 'Netflix', 'HBO', 'Disney +', 'Showmax', 'Apple Tv'].map(headerText => (
+            <TouchableOpacity
+              key={headerText}
+              style={[styles.headercontents, activeHeader === headerText && styles.activeHeader]}
+              onPress={() => setActiveHeader(headerText)}
+            >
+              <Text>{headerText}</Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
       </View>
 
       <View style={styles.notification}>
@@ -186,173 +222,3 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-between',
-    flexDirection: 'column',
-    alignItems: 'center',
-    backgroundColor: 'gray',
-  },
-  bottomContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'white',
-    shadowColor: '#000',
-  },
-  iconContainer: {
-    alignItems: 'center',
-    marginTop: 20,
-    marginBottom: 50,
-  },
-  iconText: {
-    color: '#17161A',
-    marginTop: 5,
-  },
-  iconTextHome: {
-    color: '#5303FF',
-    marginTop: 5,
-  },
-  video: {
-    width: '100%',
-    height: 790, // Updated height
-    borderRadius: 10,
-  },
-  videocontainer: {
-    borderRadius: 10,
-    width: '100%',
-    backgroundColor: 'black',
-    alignItems: 'center',
-    shadowColor: '#000',
-    position: 'relative', // Added to position the controls relative to this container
-  },
-  videocontrols: {
-    position: 'absolute',
-    bottom: 100,
-    right: 0,
-    gap: 30,
-    flexDirection: 'column', // Changed to column layout
-    alignItems: 'flex-end', // Aligned to the bottom right
-    marginRight: 10, // Added margin for spacing
-  },
-  likeCountText: {
-    color: '#fff',
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  modalContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'flex-end', // Center content vertically
-    alignItems: 'flex-end', // Center content horizontally
-  },
-  modalContent: {
-    backgroundColor: '#fff',
-    width: 'auto',
-    height: 'auto',
-    borderRadius: 10,
-    padding: 20,
-    right: 10,
-    position: 'absolute',
-    bottom: 200,
-  },
-  modalOption: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-    marginTop: 20,
-  },
-  modalOptionText: {
-    marginLeft: 10,
-    fontSize: 18,
-  },
-  outerCircle: {
-    position: 'absolute',
-    bottom: 140,
-    right: 0,
-    flexDirection: 'column', // Changed to column layout
-    alignItems: 'flex-end', // Aligned to the bottom right
-    marginRight: 10, // Added margin for spacing
-    width: 80,
-    height: 80,
-    borderRadius: 100,
-    borderWidth: 6,
-    borderColor: 'white',
-    backgroundColor: '#5303FF',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  innerCircle: {
-    width: 20,
-    height: 20,
-    borderRadius: 90,
-    borderWidth: 2,
-    borderColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  tinyCircle: {
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    borderWidth: 2,
-    borderColor: 'white',
-    position: 'absolute',
-    bottom: -10,
-    right: -6,
-  },
-  notification: {
-    position: 'absolute',
-    top: 60,
-    right: 0,
-    flexDirection: 'column', // Changed to column layout
-    alignItems: 'flex-end', // Aligned to the bottom right
-    marginRight: 10, // Added margin for spacing
-    width: 60,
-    height: 60,
-    borderRadius: 100,
-    borderColor: 'white',
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  notificationtinyCircle: {
-    width: 15,
-    height: 15,
-    borderRadius: 100,
-    backgroundColor: '#5303FF',
-    borderWidth: 2,
-    borderColor: 'white',
-    position: 'absolute',
-    top: 15,
-    right: 13,
-    zIndex: 1,
-  },
-  headercontainer: {
-    position: 'absolute',
-    top: 60,
-    flexDirection: 'column',
-    gap: 30,
-    alignItems: 'center',
-    width: 400,
-  },
-  headertitle: {
-    color: '#fff',
-    fontSize: 25,
-    fontWeight: 'bold',
-  },
-  headercontents: {
-    color: '#fff',
-    margin: 10,
-    fontSize: 18,
-  }
-});
---
+++ screens/HomeScreenStyle.js
@@ -0,0 +1,202 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: 'gray',
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    width: '100%',
+  },
+  iconContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 50,
+  },
+  iconText: {
+    color: '#17161A',
+    marginTop: 5,
+  },
+  iconTextHome: {
+    color: '#5303FF',
+    marginTop: 5,
+  },
+  videodetails: {
+    position: 'absolute',
+    bottom: 100,
+    left: 0,
+    gap: 10,
+    color: 'white',
+    flexDirection: 'column', // Changed to column layout
+    alignItems: 'flex-start', // Aligned to the bottom right
+    marginRight: 10, // Added margin for spacing
+    width: 350,
+    marginLeft: 20,
+  },
+  videodetailstext: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  videodetailstext2: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'semi-bold',
+  },
+  videoScrollView: {
+    width: '100%',
+  },
+  videocontainer: {
+    marginBottom: 5, // Adjust as needed
+    
+  },
+  videocontrols: {
+    position: 'absolute',
+    bottom: 100,
+    right: 0,
+    gap: 30,
+    flexDirection: 'column', // Changed to column layout
+    alignItems: 'flex-end', // Aligned to the bottom right
+    marginRight: 10, // Added margin for spacing
+  },
+  likeCountText: {
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  modalContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end', // Center content vertically
+    alignItems: 'flex-end', // Center content horizontally
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    width: 'auto',
+    height: 'auto',
+    borderRadius: 10,
+    padding: 20,
+    right: 10,
+    position: 'absolute',
+    bottom: 200,
+  },
+  modalOption: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  modalOptionText: {
+    marginLeft: 10,
+    fontSize: 18,
+  },
+  outerCircle: {
+    position: 'absolute',
+    bottom: 140,
+    right: 0,
+    flexDirection: 'column', // Changed to column layout
+    alignItems: 'flex-end', // Aligned to the bottom right
+    marginRight: 10, // Added margin for spacing
+    width: 80,
+    height: 80,
+    borderRadius: 100,
+    borderWidth: 6,
+    borderColor: 'white',
+    backgroundColor: '#5303FF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerCircle: {
+    width: 20,
+    height: 20,
+    borderRadius: 90,
+    borderWidth: 2,
+    borderColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tinyCircle: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: 'white',
+    position: 'absolute',
+    bottom: -10,
+    right: -6,
+  },
+  notification: {
+    position: 'absolute',
+    top: 60,
+    right: 0,
+    flexDirection: 'column', // Changed to column layout
+    alignItems: 'flex-end', // Aligned to the bottom right
+    marginRight: 10, // Added margin for spacing
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    borderColor: 'white',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  notificationtinyCircle: {
+    width: 15,
+    height: 15,
+    borderRadius: 100,
+    backgroundColor: '#5303FF',
+    borderWidth: 2,
+    borderColor: 'white',
+    position: 'absolute',
+    top: 15,
+    right: 13,
+    zIndex: 1,
+  },
+  headercontainer: {
+    position: 'absolute',
+    top: 60,
+    flexDirection: 'column',
+    gap: 30,
+    alignItems: 'center',
+    width: 400,
+    margin: 30,
+  },
+  headertitle: {
+    color: '#fff',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  headercontents: {
+    color: '#fff',
+    margin: 10,
+    fontSize: 21,
+  },
+  controlsContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent', // Set background color if needed
+  },
+  activeHeader: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#5303FF',
+    color: '#fff',
+  },
+});
+
+export default styles;
