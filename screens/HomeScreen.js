33675447bc27345f79d346daa427adc40b18835c
efexng <efeapoki2020@gmail.com>
2024-05-11 13:20:30 +0100

    chages

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,24 +11,42 @@ const videoData = [
     source: 'https://res.cloudinary.com/dsspatqxn/video/upload/v1715284562/m0n6tqk9b6jqaliqvmho.mp4',
     title: 'BEETLEJUICE BEETLEJUICE _ Official Teaser Trailer',
     description: 'In Cinemas on January 19',
+    header: 'Cinema',
   },
   {
     id: 2,
     source: 'https://res.cloudinary.com/dsspatqxn/video/upload/v1715367171/vtdpihagqxlurrymvxch.mp4',
     title: 'BEETLEJUICE BEETLEJUICE _ Official Teaser Trailer',
     description: 'In Cinemas on January 19',
+    header: 'Netflix',
   },
   {
     id: 3,
     source: 'https://res.cloudinary.com/dsspatqxn/video/upload/v1715373759/etsxpjmodymbmnngp4ip.mp4',
     title: 'BEETLEJUICE BEETLEJUICE _ Official Teaser Trailer',
     description: 'In Cinemas on January 19',
+    header: 'HBO',
   },
   {
     id: 4,
     source: 'https://res.cloudinary.com/dsspatqxn/video/upload/v1715397809/zs4rmpsaejb9qfum6ifz.mp4',
     title: 'BEETLEJUICE BEETLEJUICE _ Official Teaser Trailer',
     description: 'In Cinemas on January 19',
+    header: 'Disney +',
+  },
+  {
+    id: 5,
+    source: 'https://res.cloudinary.com/dsspatqxn/video/upload/v1715397809/zs4rmpsaejb9qfum6ifz.mp4',
+    title: 'BEETLEJUICE BEETLEJUICE _ Official Teaser Trailer',
+    description: 'In Cinemas on January 19',
+    header: 'Cinema',
+  },
+  {
+    id: 6,
+    source: 'https://res.cloudinary.com/dsspatqxn/video/upload/v1715373759/etsxpjmodymbmnngp4ip.mp4',
+    title: 'BEETLEJUICE BEETLEJUICE _ Official Teaser Trailer',
+    description: 'In Cinemas on January 19',
+    header: 'Netflix',
   },
 ];
 
@@ -42,6 +60,8 @@ const HomeScreen = () => {
   const [isPlaying, setIsPlaying] = useState({});
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   const [activeHeader, setActiveHeader] = useState('Cinema');
+  const scrollViewRef = useRef(null);
+
 
   const players = videoData.map(video =>
     useVideoPlayer(video.source, player => {
@@ -60,28 +80,40 @@ const HomeScreen = () => {
   }, []);
 
   const pauseAllExcept = index => {
-    for (let i = 0; i < players.length; i++) {
-      if (i !== index) {
-        players[i].pause();
-        setIsPlaying(prevState => ({ ...prevState, [i]: false }));
+    if (index >= 0 && index < players.length) {
+      for (let i = 0; i < players.length; i++) {
+        if (i !== index) {
+          players[i].pause();
+          setIsPlaying(prevState => ({ ...prevState, [i]: false }));
+        }
       }
+      players[index].replay();
+    } else {
+      console.error('Invalid index:', index);
     }
   };
 
+  const playVideoUnderHeader = (header, index) => {
+    const videosUnderHeader = videoData.filter(video => video.header === header);
+    if (videosUnderHeader.length > index && index >= 0) {
+      pauseAllExcept(videosUnderHeader[index].id - 1);
+      players[videosUnderHeader[index].id - 1].play();
+      setIsPlaying(prevState => ({ ...prevState, [videosUnderHeader[index].id]: true }));
+      setCurrentVideoIndex(videosUnderHeader[index].id - 1);
+    }
+  };
+  
   const handleScroll = event => {
     const contentOffsetY = event.nativeEvent.contentOffset.y;
-    const index = Math.round(contentOffsetY / 790); // Assuming each video is 790 units in height
-
+    const index = Math.round(contentOffsetY / 790);
+  
     if (index !== currentVideoIndex) {
-      pauseAllExcept(index);
-      players[index].play();
-      setIsPlaying(prevState => ({ ...prevState, [index]: true }));
-      setCurrentVideoIndex(index);
-
-      players[index].replay();
-
+      playVideoUnderHeader(activeHeader, index);
     }
   };
+  
+
+  
 
   const toggleModal = () => {
     setIsModalVisible(!isModalVisible);
@@ -110,9 +142,35 @@ const HomeScreen = () => {
     }
   };
 
+  const playFirstVideoOnHeaderChange = header => {
+    const index = videoData.findIndex(video => video.header === header);
+    if (index !== -1) {
+      pauseAllExcept(index);
+      players[index].play();
+      setIsPlaying(prevState => ({ ...prevState, [index]: true }));
+      setCurrentVideoIndex(index);
+  
+      // Reset scroll position to top
+      if (scrollViewRef.current) {
+        scrollViewRef.current.scrollTo({ x: 0, y: 0, animated: false });
+      }
+    }
+  };
+  
+
+  const pauseOtherVideosUnderHeader = (index, header) => {
+    videoData.forEach((video, idx) => {
+      if (video.header === header && idx !== index) {
+        players[idx].pause();
+        setIsPlaying(prevState => ({ ...prevState, [idx]: false }));
+      }
+    });
+  };
+
   return (
     <View style={styles.container}>
       <ScrollView
+        ref={scrollViewRef}
         style={styles.videoScrollView}
         showsVerticalScrollIndicator={false}
         pagingEnabled
@@ -120,66 +178,74 @@ const HomeScreen = () => {
       >
         {videoData.map((video, index) => (
           <View key={video.id} style={styles.videocontainer}>
-            <View style={{ backgroundColor: 'black', borderRadius: 5 }}>
-              <VideoView
-                ref={ref}
-                style={{ height: 790, borderRadius: 10 }}
-                player={players[index]}
-                contentFit="cover"
-                contentPosition="center"
-                nativeControls={false}
-                shouldPlay={isPlaying[index]}
-                resizeMode="cover"
-              />
-              <View style={styles.controlsContainer}>
-                <TouchableOpacity
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}
-                  onPress={() => {
-                    if (isPlaying[index]) {
-                      players[index].pause();
-                    } else {
-                      players[index].play();
+            {video.header === activeHeader && (
+              <View style={{ backgroundColor: 'black', borderRadius: 5 }}>
+                <VideoView
+                  ref={ref}
+                  style={{ height: 790, borderRadius: 10 }}
+                  player={players[index]}
+                  contentFit="cover"
+                  contentPosition="center"
+                  nativeControls={false}
+                  shouldPlay={isPlaying[index]}
+                  resizeMode="cover"
+                  onPlaybackStatusUpdate={status => {
+                    if (!status.isPlaying) {
+                      setIsPlaying(prevState => ({ ...prevState, [index]: false }));
                     }
-                    setIsPlaying(prevState => ({ ...prevState, [index]: !prevState[index] }));
                   }}
                 />
-              </View>
-              <View style={styles.videodetails}>
-                <Text style={styles.videodetailstext}>{video.title}</Text>
-                <Text style={styles.videodetailstext2}>{video.description}</Text>
-              </View>
-              <View style={styles.videocontrols}>
-                <TouchableOpacity onPress={() => toggleLike(video.id)}>
-                  <Ionicons
-                    name={isLiked[video.id] ? 'heart' : 'heart-outline'}
-                    size={30}
-                    color={isLiked[video.id] ? 'red' : '#fff'}
+                <View style={styles.controlsContainer}>
+                  <TouchableOpacity
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}
+                    onPress={() => {
+                      if (isPlaying[index]) {
+                        players[index].pause();
+                      } else {
+                        players[index].play();
+                      }
+                      setIsPlaying(prevState => ({ ...prevState, [index]: !prevState[index] }));
+                      pauseOtherVideosUnderHeader(index, activeHeader);
+                    }}
                   />
-                  {likeCount[video.id] > 0 && (
-                    <Text style={styles.likeCountText}>{formatLikeCount(likeCount[video.id])}</Text>
-                  )}
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => toggleVolume(video.id)}>
-                  <Ionicons
-                    name={isVolumeMuted[video.id] ? 'volume-mute-outline' : 'volume-high-outline'}
-                    size={30}
-                    color="#fff"
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={toggleModal}>
-                  <Ionicons
-                    name="ellipsis-vertical-outline"
-                    size={30}
-                    color="#fff"
-                  />
-                </TouchableOpacity>
+                </View>
+                <View style={styles.videodetails}>
+                  <Text style={styles.videodetailstext}>{video.title}</Text>
+                  <Text style={styles.videodetailstext2}>{video.description}</Text>
+                </View>
+                <View style={styles.videocontrols}>
+                  <TouchableOpacity onPress={() => toggleLike(video.id)}>
+                    <Ionicons
+                      name={isLiked[video.id] ? 'heart' : 'heart-outline'}
+                      size={30}
+                      color={isLiked[video.id] ? 'red' : '#fff'}
+                    />
+                    {likeCount[video.id] > 0 && (
+                      <Text style={styles.likeCountText}>{formatLikeCount(likeCount[video.id])}</Text>
+                    )}
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => toggleVolume(video.id)}>
+                    <Ionicons
+                      name={isVolumeMuted[video.id] ? 'volume-mute-outline' : 'volume-high-outline'}
+                      size={30}
+                      color="#fff"
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={toggleModal}>
+                    <Ionicons
+                      name="ellipsis-vertical-outline"
+                      size={30}
+                      color="#fff"
+                    />
+                  </TouchableOpacity>
+                </View>
               </View>
-            </View>
+            )}
           </View>
         ))}
       </ScrollView>
@@ -191,13 +257,17 @@ const HomeScreen = () => {
             <TouchableOpacity
               key={headerText}
               style={[styles.headercontents, activeHeader === headerText && styles.activeHeader]}
-              onPress={() => setActiveHeader(headerText)}
+              onPress={() => {
+                setActiveHeader(headerText);
+                playFirstVideoOnHeaderChange(headerText);
+              }}
             >
               <Text>{headerText}</Text>
             </TouchableOpacity>
           ))}
         </ScrollView>
       </View>
+
 
       <View style={styles.notification}>
         <View style={styles.notificationtinyCircle} />
