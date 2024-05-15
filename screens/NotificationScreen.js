82324040bba57dd25edfdcc78cb9d3837f1a3f32
efexng <efeapoki2020@gmail.com>
2024-05-16 00:27:39 +0100

    changes

--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -1,9 +1,10 @@
 import React, { useState, } from 'react';
-import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { ScrollView } from 'react-native-gesture-handler';
-
+import styles from './NotificationStyle';
+import { BackIcon} from './icons';
 
 
 
@@ -13,14 +14,19 @@ const notifications = [
         date: '06/05/2024',
         notifications: [
             {
-                logoText: 'G',
+                image: require('../assets/genesis-icon.png'), 
+                images: [
+                    require('../assets/if.png'),
+                    require('../assets/mad-max.png'),
+                    require('../assets/bad-boys.png'),
+                ],
                 company: 'Genesis Cinema',
-                message: 'Enjoy non-stop happiness with our selected up to date movie collections curated just for you and ...',
+                message: 'Enjoy non-stop happiness with our selected up to date movie collections curated just f...',
                 time: '11:50 AM',
                 type: 'unread',
             },
             {
-                logoText: 'G',
+                image: require('../assets/genesis-icon.png'), 
                 company: 'Genesis Cinema',
                 message: 'Enjoy non-stop happiness with our selected up to date movie collections curated just for you and ...',
                 time: '11:50 AM',
@@ -32,11 +38,18 @@ const notifications = [
         date: '07/05/2024',
         notifications: [
             {
-                logoText: 'A',
-                company: 'Genesis Cinema',
-                message: 'Enjoy non-stop happiness with our selected up to date movie collections curated just for you and ...',
+                image: require('../assets/Netflix-icon.png'), 
+                company: 'Netflix',
+                message: 'The unbeatable offer of the summer: Netflix Premium for just $1.99/month for 3 months, starts on May 20.',
                 time: '11:50 AM',
                 type: 'read',
+            },
+            {
+                image: require('../assets/primevideo-icon.png'), 
+                company: 'Prime Video',
+                message: 'The highly anticipated fourth season of the Emmy-nominated hit drama series The Boys will premiere on Prime Video on June 13. ',
+                time: '11:50 AM',
+                type: 'unread',
             },
         ]
     },
@@ -81,7 +94,7 @@ const truncateText = (text, maxLength) => {
         
             <View style={styles.header}>
                 <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <Ionicons style={styles.back} name="arrow-back" size={20} color="black" />
+                   <BackIcon />
                 </TouchableOpacity>
                 <Text style={styles.notificationText}>Notification</Text>
             </View>
@@ -128,20 +141,20 @@ const truncateText = (text, maxLength) => {
                         <View key={innerIndex} style={styles.notificationboxcontent}>
                             <View style={styles.notificationlogo}>
                                 <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start' }}>
-                                    <View style={styles.logoCircle}>
-                                        <Text style={styles.logoText}>{notification.logoText}</Text>
-                                    </View>
+                                <View style={styles.shadowContainer}>
+                                    <Image style={styles.streamLogo} source={notification.image} />
+                                    <Image source={notification.images} />
+                                </View>
                                     <Text style={notification.type === 'unread' ? styles.notificationboxmsgtextunread : styles.notificationboxmsgtext}>
                                         {notification.company}
                                     </Text>
                                 </View>
                                 <Ionicons name="ellipsis-horizontal-outline" size={24} color="#56545C" />
                             </View>
-                            <View style={styles.notificationboxmsg}>
-                                <Text style={styles.notificationboxmsgtextbottom}>
-                                    {truncateText(notification.message, 100)}
+                                <Text style={notification.type === 'unread' ? styles.notificationboxmsgtextbottom :  styles.notificationboxmsgtextbottomgray}>
+                                    {truncateText(notification.message, 86)}
                                 </Text>
-                            </View>
+              
                             <View style={styles.notificationboxtime}>
                                 <Text style={styles.notificationboxmsgtime}>{notification.time}</Text>
                             </View>
@@ -158,160 +171,5 @@ const truncateText = (text, maxLength) => {
     );
 };
 
-
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '##F5F5F5'
-    },
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        margin: 20,
-    },
-    notificationText: {
-        fontSize: 16,
-        fontWeight: 'semibold',
-        textAlign: 'center',
-        flex: 1,
-    },
-    back: {
-        marginRight: 16,
-    },
-    separator: {
-        borderBottomWidth: .5,
-        borderBottomColor: 'gray',
-        width: '100%',
-        alignSelf: 'center',
-    },
-    statusContainer: {
-        marginTop: 16,
-        justifyContent: 'center', // Center content vertically
-        paddingHorizontal: 70, // Add space from left and right
-    },
-    status: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: 50,
-        backgroundColor: 'white',
-        borderRadius: 10,
-        paddingHorizontal: 20, // Add space from left and right
-    },
-    statusText: {
-        color: 'black',
-        fontSize: 16,
-    },
-    statuslightContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    statuslight: {
-        fontSize: 5,
-        color: '#07F411',
-        marginLeft: 2,
-    },
-    boxContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#fff',
-        marginVertical: 20,
-        marginHorizontal: 40,
-        borderRadius: 10, // Add border radius for rounded corners
-    },
-    boxItem: {
-        flex: 1,
-        alignItems: 'center',
-        marginVertical: 6,
-        marginHorizontal: 6,
-        paddingVertical: 10,
-        borderRadius: 10,
-    },
-    boxText: {
-        fontSize: 12,
-    },
-    unread: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    notificationbox: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginBottom: 20,
-    },
-    notificationboxcontent: {
-        flexDirection: 'column',
-        backgroundColor: 'white',
-        borderRadius: 10,
-        width: '90%',
-        marginTop: 10,
-    },
-    notificationlogo: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        margin: 5,
-    },
-    notificationlogo2: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        margin: 5,
-        marginTop: 15,
-    },
-    notificationboxmsg: {
-        marginLeft: 45,
-        width: 280,
-        height: 29,
-    },
-    notificationboxmsgtext: {
-        fontSize: 16,
-        fontWeight: '600',
-        marginLeft: 10,
-        color: 'gray', 
-    },
-    notificationboxmsgtextunread: {
-        fontSize: 16,
-        fontWeight: '600',
-        marginLeft: 10,
-        color: 'black', 
-    },    
-    notificationboxmsgtextbottom: {
-        fontSize: 12,
-        fontWeight: '400',
-    },
-    notificationboxtime: {
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        margin: 5,
-        color: 'gray',
-    },
-    notificationboxmsgtime: {
-        fontSize: 12,
-        fontWeight: '400',
-        marginLeft: 10,
-        color: 'gray',
-    },
-    logoText: {
-        fontWeight: '900',
-        color: '#fff',
-        fontSize: 20, 
-    },
-    logoCircle: {
-        alignItems: 'center', 
-        justifyContent: 'center',
-        backgroundColor: '#808080',
-        borderRadius: '20', 
-        width: 30, 
-        height: 30, 
-    },
-});
 
 export default NotificationScreen;
---
+++ screens/NotificationStyle.js
@@ -0,0 +1,166 @@
+import { StyleSheet } from 'react-native';
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '##F5F5F5'
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 20,
+    },
+    notificationText: {
+        fontSize: 16,
+        fontFamily: 'Outfit_500Medium',
+        textAlign: 'center',
+        flex: 1,
+    },
+    back: {
+        marginRight: 16,
+    },
+    separator: {
+        borderBottomWidth: .5,
+        borderBottomColor: 'gray',
+        width: '100%',
+        alignSelf: 'center',
+    },
+    statusContainer: {
+        marginTop: 16,
+        justifyContent: 'center', // Center content vertically
+        paddingHorizontal: 70, // Add space from left and right
+    },
+    status: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: 50,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        paddingHorizontal: 20, // Add space from left and right
+    },
+    statusText: {
+        color: 'black',
+        fontSize: 16,
+    },
+    statuslightContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    statuslight: {
+        fontSize: 5,
+        color: '#07F411',
+        marginLeft: 2,
+    },
+    boxContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        marginVertical: 20,
+        marginHorizontal: 40,
+        borderRadius: 10, // Add border radius for rounded corners
+    },
+    boxItem: {
+        flex: 1,
+        alignItems: 'center',
+        marginVertical: 6,
+        marginHorizontal: 6,
+        paddingVertical: 10,
+        borderRadius: 10,
+    },
+    boxText: {
+        fontSize: 14,
+        fontFamily: 'Outfit_400Regular'
+    },
+    unread: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    notificationbox: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 20,
+    },
+    notificationboxcontent: {
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        width: '90%',
+        marginTop: 10,
+    },
+    notificationlogo: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: 10,
+    },
+    notificationlogo2: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: 5,
+        marginTop: 15,
+    },
+    notificationboxmsgtext: {
+        fontSize: 14,
+        fontFamily: 'Outfit_600SemiBold',
+        marginLeft: 10,
+        color: 'gray', 
+    },
+    notificationboxmsgtextunread: {
+        fontSize: 14,
+        fontFamily: 'Outfit_600SemiBold',
+        marginLeft: 10,
+        color: 'black', 
+    },    
+    notificationboxmsgtextbottom: {
+        fontSize: 14,
+        fontFamily: 'Outfit_400Regular',
+        color: 'black',
+        width: 270,
+        marginLeft: 45,
+    },
+    notificationboxmsgtextbottomgray: {
+        fontSize: 14,
+        fontFamily: 'Outfit_400Regular',
+        color: 'gray',
+        width: 270,
+        marginLeft: 45,
+    },
+    notificationboxtime: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        margin: 5,
+        color: 'gray',
+    },
+    notificationboxmsgtime: {
+        fontSize: 12,
+        fontWeight: '400',
+        marginLeft: 10,
+        color: 'gray',
+    },
+    shadowContainer: {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    streamLogo: {
+        width: 24,
+        height: 24,
+        borderRadius: 50,
+    },
+});
+
+export default styles;
